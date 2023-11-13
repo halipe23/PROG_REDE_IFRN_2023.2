7d9a0b13d4071372a7efad8c165c3e0c611a4cc6
halipe23 <80537218+halipe23@users.noreply.github.com>
2023-11-12 21:27:55 -0300

    Update Dados_Arquivo.py
    Usando PIL

--- Metadados/Dados_Arquivo.py
+++ Metadados/Dados_Arquivo.py
@@ -1,22 +1,43 @@
 import os
-import requests
-import json
+
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+def obter_metadados_imagem(caminho_arquivo):
+    try:
+        imagem = Image.open(caminho_arquivo)
+        exif_data = imagem._getexif()
+
+        if exif_data is not None:
+            metadados = {}
+            for tag, valor in exif_data.items():
+                tag_nome = TAGS.get(tag, tag)
+                metadados[tag_nome] = valor
+
+            return metadados
+        else:
+            print(f"A imagem {caminho_arquivo} não possui metadados EXIF.")
+            return None
+    except Exception as e:
+        print(f"Erro ao obter metadados da imagem {caminho_arquivo}: {str(e)}")
+        return None
+
+def imprimir_metadados(metadados):
+    if metadados:
+        print(f"Largura da foto: {metadados.get('ExifImageWidth', 'N/A')}")
+        print(f"Altura da foto: {metadados.get('ExifImageHeight', 'N/A')}")
+        print(f"Fabricante da câmera: {metadados.get('Make', 'N/A')}")
+        print(f"Modelo da câmera: {metadados.get('Model', 'N/A')}")
+        print(f"Data/Hora de captura: {metadados.get('DateTimeOriginal', 'N/A')}")
+        print("\n")
 
 def ler_metadados_imagem(diretorio):
     for arquivo in os.listdir(diretorio):
-        if arquivo.endswith(".jpg") or arquivo.endswith(".jpeg"):
+        nome_arquivo, extensao = os.path.splitext(arquivo)
+        if extensao.lower() in [".jpg", ".jpeg"]:
             caminho_arquivo = os.path.join(diretorio, arquivo)
-            with open(caminho_arquivo, "rb") as imagem:
-                dados_imagem = imagem.read()
-                url = "https://api.metadados.com/analyze"
-                headers = {"Content-Type": "application/octet-stream"}
-                response = requests.post(url, headers=headers, data=dados_imagem)
-                if response.status_code == 200:
-                    metadados = json.loads(response.text)
-                    print(f"Metadados da imagem {arquivo}:")
-                    print(json.dumps(metadados, indent=4))
-                else:
-                    print(f"Erro ao obter metadados da imagem {arquivo}")
+            metadados = obter_metadados_imagem(caminho_arquivo)
+            imprimir_metadados(metadados)
 
 diretorio = input("Digite o nome do diretório: ")
 ler_metadados_imagem(diretorio)
