94c1c5e88cec1a9e09886a08b8add62b45374260
halipe23 <80537218+halipe23@users.noreply.github.com>
2024-01-10 09:41:23 -0300

    Update bot.py

--- PROJETO/PROJETO FINAL/bot.py
+++ PROJETO/PROJETO FINAL/bot.py
@@ -1,19 +1,17 @@
 import telebot
 import socket
 
-KEY_API = ""
+KEY_API = "6849345429:AAFDRM9v0KkNKjlLH59epOcwvcCntyH1fZU"
 bot = telebot.TeleBot(KEY_API)
 
 def send_command_to_server(command):
-    server_host = '127.0.0.1'      
+    server_host = '0.0.0.0'      
     server_port = 5757  
 
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((server_host, server_port))
-
-    client.send(command.encode('utf-8'))
-    response = client.recv(4096).decode('utf-8')
-    client.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect((server_host, server_port))
+        client.send(command.encode('utf-8'))
+        response = client.recv(4096).decode('utf-8')
 
     return response
 
@@ -42,11 +40,7 @@ def listclient(mensagem):
     response = send_command_to_server('/listclient')
     bot.reply_to(mensagem, response)
 
-def VERIFICAR(mensagem):
-    return True
-
-@bot.message_handler(func=VERIFICAR)
-def RESP_PADRAO(mensagem):
+def default_response(mensagem):
     texto = "Bem-vindo ao Bot Gaga_i_rejo, as opções disponíveis são:\n\n" \
             "/info_h - Informações do hardware onde o servidor está sendo executado.\n" \
             "/info_p - Lista de programas instalados no servidor.\n" \
@@ -54,5 +48,9 @@ def RESP_PADRAO(mensagem):
             "/info_u - Informações detalhadas do usuário logado.\n" \
             "/listclient - Lista dos agentes online com informações básicas."
     bot.reply_to(mensagem, texto)
+
+@bot.message_handler(func=default_response)
+def default_response(mensagem):
+    default_response(mensagem)
 
 bot.polling()
