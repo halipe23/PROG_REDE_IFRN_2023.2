9f41b904b5e8d1a62d1a35b0d541a2479ea87042
halipe23 <80537218+halipe23@users.noreply.github.com>
2024-01-10 09:41:50 -0300

    Update Server.py

--- PROJETO/PROJETO FINAL/Server.py
+++ PROJETO/PROJETO FINAL/Server.py
@@ -1,10 +1,8 @@
 import socket
 import threading
-import platform
 
 def handle_client(client_socket):
     while True:
-
         command = input("Digite um comando para enviar ao cliente ('exit' para sair): ")
 
         if command == 'exit':
@@ -18,22 +16,27 @@ def handle_client(client_socket):
     client_socket.close()
 
 def start_server():
+    host = '0.0.0.0'
+    port = 5757
 
-    host = '127.0.0.1'  
-    port = 5757  
-    
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((host, port))
 
     server.listen(5)
-    print(f"[*] Servidor ouvindo em {host}:{port}")
-    
+    print(f"Servidor ouvindo em {host}:{port}")
+
     while True:
-        client_socket, addr = server.accept()
-        print(f"[*] Conexão recebida de {addr[0]}:{addr[1]}")
-       
-        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
-        client_thread.start()
+        try:
+            client_socket, addr = server.accept()
+            print(f"Conexão recebida de {addr[0]}:{addr[1]}")
+
+            client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+            client_thread.start()
+        except KeyboardInterrupt:
+            print("Servidor encerrado.")
+            break
+        except Exception as e:
+            print(f"Erro ao aceitar conexão: {e}")
 
 if __name__ == "__main__":
     start_server()
